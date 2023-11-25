27f5425a8ea84d9eb0f65cad86f4fbc07f436083
Brooke Allen <allenmbrooke@gmail.com>
2023-11-24 21:46:41 -0600

    Fixed typo in output dir

--- bigscape.sh
+++ bigscape.sh
@@ -11,7 +11,7 @@ source activate bigscape
 
 #Input/output paths
 antismash_out="/ddn/home12/r2620/slu_fresh/antismash/as_output/"
-bs_out="ddn/home12/r2620/slu_fresh/bigscape/output"
+bs_out="/ddn/home12/r2620/slu_fresh/bigscape/output" 
 
 #Run BigScape analysis on .gbk files from antismash
 /ddn/home12/r2620/BiG-SCAPE-1.1.5/bigscape.py -i "${antismash_out}" -o "${bs_out}"
